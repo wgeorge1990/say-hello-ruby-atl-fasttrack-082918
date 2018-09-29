352562a8582d133431436b2de166f5e53b560cae
william george <williameverettgeorge@icloud.com>
2018-09-29 16:09:23 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,12 +1,6 @@
-def say_hello(name="william")
+def say_hello(name="Hello Ruby Programmer!")
  puts name
 end
 
 say_hello("Gabriela")
 
-def add_numbers(num1, num2)
-  puts num1+num2
-end
-add_numbers(5,6)
-
-say_hello()
